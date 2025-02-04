b1800d32b873581931534b469410b75548e403ee
PeterGamez <chanakan.kea@gmail.com>
2025-02-04 15:34:05 +0700

    Add API configuration and controllers for Spring Boot application

---
+++ server/src/main/java/com/mechat/restAPI/APIController.java
@@ -0,0 +1,12 @@
+package com.mechat.restAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class APIController {
+    @GetMapping()
+    public String index() {
+        return "Hello, Spring Boot API!";
+    }
+}
