43c92c8d04822464d9bfbf79129d3194eea2d4f1
Soungwut Konak <soungwut.k@ku.th>
2025-02-09 18:46:44 +0700

    Add theme support based on Windows settings with dark and light styles

---
+++ client/src/main/java/com/mechat/WindowsSettings.java
@@ -0,0 +1,32 @@
+package com.mechat;
+
+import java.io.IOException;
+
+public class WindowsSettings {
+
+    public static String getWindowsTheme() {
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "reg",
+                    "query",
+                    "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+                    "/v",
+                    "AppsUseLightTheme"
+            );
+            Process process = processBuilder.start();
+            process.waitFor();
+            String result = new String(process.getInputStream().readAllBytes());
+
+            // if AppsUseLightTheme is 0 (dark mode)
+            // if AppsUseLightTheme is 1 (light mode)
+            if (result.contains("0x0")) {
+                return "dark";
+            } else {
+                return "light";
+            }
+        } catch (InterruptedException | IOException e) {
+            System.out.println(e.getMessage());
+            return "light";
+        }
+    }
+}
