340f03164ace75717f7040ddb2e9d9294e10a489
PeterGamez <chanakan.kea@gmail.com>
2025-02-12 18:15:10 +0700

    Add BaseDTO and UserDTO classes for user data transfer

---
+++ server/src/main/java/com/mechat/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package com.mechat.dto;
+
+public class UserDTO extends BaseDTO {
+    private String username;
+    private String password;
+    private String displayName;
+    private String avatar;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
