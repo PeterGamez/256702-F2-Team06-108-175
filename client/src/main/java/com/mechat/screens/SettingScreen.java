892f8f0fd5f234bdfbc2dd5cde28b9b742b5580c
Soungwut Konak <soungwut.k@ku.th>
2025-02-23 18:51:42 +0700

    Enhance SettingScreen layout with user information display and improved padding

--- client/src/main/java/com/mechat/screens/SettingScreen.java
+++ client/src/main/java/com/mechat/screens/SettingScreen.java
@@ -59,15 +59,57 @@ public class SettingScreen implements ScreenInterface {
         VBox content = new VBox();
         content.setAlignment(Pos.TOP_CENTER);
         content.setSpacing(40);
-        content.setPadding(new Insets(40, 0, 0, 0));
+        content.setPadding(new Insets(100, 0, 0, 0));
 
         ImageView avatar = new ImageView(new Image("images/profile-icon.png"));
         avatar.setFitWidth(100);
         avatar.setFitHeight(100);
 
-        content.getChildren().addAll(avatar);
-        content.setSpacing(40);
+        // HBox profileBox = new HBox();
+        // profileBox.setPadding(new Insets(0, 100, 0, 100));
+        // Label title = new Label("Display Name");
+        // title.getStyleClass().add("settings-label");
+        // Label name = new Label(user);
+        // name.getStyleClass().add("settings-label");
+        // profileBox.getChildren().addAll(title, name);
+        // profileBox.setAlignment(Pos.CENTER);
+        // profileBox.setSpacing(60);
+        // HBox UUIDBox = new HBox();
+        // UUIDBox.setPadding(new Insets(0, 100, 0, 100));
+        // Label uTitle = new Label("UUID");
+        // uTitle.getStyleClass().add("settings-label");
+        // Label uuid = new Label(uid);
+        // uuid.getStyleClass().add("settings-label");
+        // UUIDBox.getChildren().addAll(uTitle, uuid);
+        // UUIDBox.setAlignment(Pos.CENTER);
+        // UUIDBox.setSpacing(60);
 
+        // content.getChildren().addAll(avatar, profileBox, UUIDBox);
+
+        VBox titleBox = new VBox();
+        titleBox.setSpacing(40);
+        titleBox.setAlignment(Pos.CENTER_LEFT);
+        Label userTitle = new Label("Display User");
+        userTitle.getStyleClass().add("settings-label");
+        Label UUIDTitle = new Label("UUID");
+        UUIDTitle.getStyleClass().add("settings-label");
+        titleBox.getChildren().addAll(userTitle, UUIDTitle);
+
+        VBox informationBox = new VBox();
+        informationBox.setSpacing(40);
+        informationBox.setAlignment(Pos.CENTER_RIGHT);
+        Label userLabel = new Label(user);
+        userLabel.getStyleClass().add("settings-label");
+        Label UUIDLabel = new Label(uid);
+        UUIDLabel.getStyleClass().add("settings-label");
+        informationBox.getChildren().addAll(userLabel, UUIDLabel);
+
+        HBox mergedBox = new HBox();
+        mergedBox.setAlignment(Pos.CENTER);
+        mergedBox.setSpacing(40);
+        mergedBox.getChildren().addAll(titleBox, informationBox);
+
+        content.getChildren().addAll(avatar, mergedBox);
         BorderPane root = new BorderPane();
         root.setLeft(menu);
         root.setCenter(content);
