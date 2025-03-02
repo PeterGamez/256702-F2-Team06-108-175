0dca3bbf26c52373dd288f36961114e3e0954404
PeterGamez <chanakan.kea@gmail.com>
2025-03-02 22:31:13 +0700

    Load environment variables using Dotenv in the main server application

--- server/src/main/java/com/mechat/Server.java
+++ server/src/main/java/com/mechat/Server.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.mechat")
 public class Server {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication app = new SpringApplication(Server.class);
         app.setBanner(new CustomBanner());
         app.run(args);
