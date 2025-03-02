db740c37a0b6710d6ad95612965c07579f95acad
PeterGamez <chanakan.kea@gmail.com>
2025-03-02 10:49:52 +0700

    Add component scanning for the Mechat package in the Server class

--- server/src/main/java/com/mechat/Server.java
+++ server/src/main/java/com/mechat/Server.java
@@ -2,8 +2,10 @@ package com.mechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mechat")
 public class Server {
 
     public static void main(String[] args) {
