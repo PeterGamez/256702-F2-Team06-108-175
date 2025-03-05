3891ca0da1bbf8ba253eb42a462b417653993186
PeterGamez <chanakan.kea@gmail.com>
2025-03-05 09:25:24 +0700

    Add chat member repository methods and enhance chat service functionality

--- server/src/main/java/com/mechat/repository/ChatMemberRepository.java
+++ server/src/main/java/com/mechat/repository/ChatMemberRepository.java
@@ -1,9 +1,14 @@
 package com.mechat.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mechat.entity.ChatMember;
 
 public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
-
+    @Query("SELECT cm FROM ChatMember cm WHERE cm.chat.id = :chatId AND cm.user.id != :userId")
+    List<ChatMember> findByChatNotUser(@Param("chatId") Long chatId, @Param("userId") Long userId);
 }
