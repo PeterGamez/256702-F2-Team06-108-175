0985f59e04464dbec178959c221dcb951fe9c94d
Soungwut Konak <soungwut.k@ku.th>
2025-03-25 15:28:56 +0700

    Implement AddFriendScreen; update App to use AddFriendScreen as main interface; add UI components and styles for adding friends

--- client/src/main/java/com/mechat/App.java
+++ client/src/main/java/com/mechat/App.java
@@ -1,7 +1,7 @@
 package com.mechat;
 
 import com.mechat.interfaces.ScreenInterface;
-import com.mechat.screens.MainScreen;
+import com.mechat.screens.AddFriendScreen;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
@@ -27,7 +27,7 @@ public class App extends Application {
 
         new ScreenHandler(stage, windowWidth, windowHeight);
 
-        ScreenInterface mainScreen = new MainScreen();
+        ScreenInterface mainScreen = new AddFriendScreen();
         ScreenHandler.setScreen(mainScreen);
 
         stage.show();
--- client/src/main/java/com/mechat/screens/AddFriendScreen.java
+++ client/src/main/java/com/mechat/screens/AddFriendScreen.java
@@ -1,17 +1,91 @@
 package com.mechat.screens;
 
+import com.mechat.ScreenHandler;
 import com.mechat.interfaces.ScreenInterface;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class AddFriendScreen implements ScreenInterface {
 
     @Override
     public Parent createContent() {
-        VBox root = new VBox();
+        BorderPane root = new BorderPane();
 
+        //header
+        HBox header = new HBox();
+        header.getStyleClass().add("header");
+        header.setAlignment(Pos.CENTER_LEFT);
 
+        Button backButton = createImageButton("images/back-button.png", 30, 30, "back-button");
+        backButton.setOnAction(e -> {
+            ScreenHandler.setScreen(new MainChatScreen());
+        });
+
+        Label title = new Label("Add Friend");
+        title.getStyleClass().add("misc-label");
+
+        StackPane titlePane = new StackPane(title);
+        titlePane.setAlignment(Pos.CENTER);
+        HBox.setHgrow(titlePane, Priority.ALWAYS);
+
+        header.getChildren().addAll(backButton, titlePane);
+        header.setPadding(new Insets(0, 70, 0, 0));
+
+        //content
+        VBox box = new VBox();
+
+        TextField friendNameField = new TextField();
+        friendNameField.setPromptText("Enter friend's name");
+        friendNameField.getStyleClass().add("friend-name-field");
+
+        ImageView imageView = new ImageView("images/profile-icon.png");
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+
+        Button addFriendButton = new Button("Add Friend");
+        addFriendButton.getStyleClass().add("add-friend-button");
+
+        box.getChildren().addAll(friendNameField, imageView, addFriendButton);
+        box.setAlignment(Pos.TOP_CENTER);
+        box.setPadding(new Insets(40, 60, 0, 60));
+        box.setSpacing(30);
+
+        root.setTop(header);
+        root.setCenter(box);
         return root;
+    }
+
+    private Button createImageButton(String imagePath, double width, double height, String styleClass) {
+        Button button = new Button();
+        ImageView imageView = createImageView(imagePath, width, height);
+        button.setGraphic(imageView);
+        button.getStyleClass().add(styleClass);
+        return button;
+    }
+
+    private ImageView createImageView(String imagePath, double width, double height) {
+        Image image = new Image(imagePath);
+        ImageView imageView = new ImageView(new javafx.scene.image.Image(imagePath));
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+
+        return imageView;
+    }
+
+    private void backEvent(MouseEvent e) {
+        ScreenHandler.setScreen(new MainChatScreen());
     }
 }
