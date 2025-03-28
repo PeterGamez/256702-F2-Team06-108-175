f1cfc8a564aa05d80389cdf5aeea7d6d6f818799
PeterGamez <chanakan.kea@gmail.com>
2025-03-28 16:11:27 +0700

    Refactor AuthController to store plain password; simplify Crypt class by removing exception handling in encrypt and decrypt methods

--- server/src/main/java/com/mechat/utils/Crypt.java
+++ server/src/main/java/com/mechat/utils/Crypt.java
@@ -7,21 +7,12 @@ public class Crypt {
     private static final int saltRounds = 10;
 
     public static String encrypt(String data) {
-        try {
-            String salt = BCrypt.gensalt(saltRounds);
-            String hashedData = BCrypt.hashpw(data, salt);
-            return hashedData;
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
+        String salt = BCrypt.gensalt(saltRounds);
+        String hashedData = BCrypt.hashpw(data, salt);
+        return hashedData;
     }
 
     public static boolean decrypt(String data, String hashedData) {
-        try {
-            return BCrypt.checkpw(data, hashedData);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            return false;
-        }
+        return BCrypt.checkpw(data, hashedData);
     }
 }
