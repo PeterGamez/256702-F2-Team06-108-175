8816754bbaca18d7db4580417bfe5912a3d4f60e
PeterGamez <chanakan.kea@gmail.com>
2025-03-29 18:11:22 +0700

    Refactor RestApiService: add logging for connection, login, registration, and user retrieval

--- client/src/main/java/com/mechat/service/RestApiService.java
+++ client/src/main/java/com/mechat/service/RestApiService.java
@@ -3,6 +3,8 @@ package com.mechat.service;
 import java.time.Duration;
 import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.mechat.MakeCache;
@@ -11,19 +13,27 @@ import reactor.core.publisher.Mono;
 
 public class RestApiService {
 
+    private static final Logger log = LoggerFactory.getLogger(RestApiService.class);
+
     private static WebClient webClient;
 
     public static void connect() {
+        log.info("Connecting to server...");
+
         String serverIp = Objects.toString(MakeCache.getServer().get("serverIp"));
         String serverPort = Objects.toString(MakeCache.getServer().get("serverPort"));
         RestApiService.webClient = WebClient.builder().baseUrl("http://" + serverIp + ":" + serverPort).build();
+
+        log.info("Connected to server: " + serverIp + ":" + serverPort);
     }
 
     public static void disconnect() {
+        log.info("Disconnecting from server...");
         RestApiService.webClient = null;
     }
 
     public static Mono<String> getConnection(String serverIp, String serverPort) throws Exception {
+        log.info("Check Connecting to server: " + serverIp + ":" + serverPort);
         WebClient webClient = WebClient.builder().baseUrl("http://" + serverIp + ":" + serverPort).build();
 
         return webClient.get()
@@ -35,6 +45,7 @@ public class RestApiService {
     }
 
     public static Mono<String> login(String username, String password) throws Exception {
+        log.info("Login: " + username + " " + password);
         String body = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
 
         return webClient.post()
@@ -51,6 +62,7 @@ public class RestApiService {
     }
 
     public static Mono<String> register(String username, String password) throws Exception {
+        log.info("Register: " + username + " " + password);
         String body = "{\"username\": \"" + username + "\", \"password\": \"" + password + "\"}";
 
         return webClient.post()
@@ -67,6 +79,7 @@ public class RestApiService {
     }
 
     public static Mono<String> getUser(String userId) throws Exception {
+        log.info("Get user: " + userId);
         return webClient.get()
                 .uri("/v1/user/" + userId)
                 .retrieve()
@@ -79,6 +92,7 @@ public class RestApiService {
     }
 
     public static Mono<String> getUserByUsername(String username) throws Exception {
+        log.info("Get user by username: " + username);
         return webClient.get()
                 .uri("/v1/user/search/" + username)
                 .retrieve()
@@ -91,6 +105,7 @@ public class RestApiService {
     }
 
     public static Mono<String> getChat(String chatId) throws Exception {
+        log.info("Get chat: " + chatId);
         return webClient.get()
                 .uri("/v1/chat/" + chatId)
                 .retrieve()
