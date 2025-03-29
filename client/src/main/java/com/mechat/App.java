54cb110043768326674307d5319b25e9db276fb8
PeterGamez <chanakan.kea@gmail.com>
2025-03-29 21:40:57 +0700

    Add application icon to main stage

--- client/src/main/java/com/mechat/App.java
+++ client/src/main/java/com/mechat/App.java
@@ -3,6 +3,7 @@ package com.mechat;
 import com.mechat.controller.home.MainController;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -22,6 +23,7 @@ public class App extends Application {
         double windowHeight = screenHeight * 0.7;
 
         stage.setTitle("MeChat");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/main-icon.png")));
 
         new ScreenHandler(stage, windowWidth, windowHeight);
         new MakeCache();
